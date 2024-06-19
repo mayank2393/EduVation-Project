10d0687fac2919abbcc56ed8e32a036e4eeecc9f
Mayank Tripathi <mayankchamps27@gmail.com>
2024-06-19 23:47:10 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: "http://localhost:3000",
+		origin: "https://ed-tech-project-delta.vercel.app",
 		credentials: true,
 	})
 )
